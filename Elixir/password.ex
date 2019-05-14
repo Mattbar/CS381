fd1dd3298df8d49520c7cbad3b35148961779118
Mackenzie <therealbakoonzar@gmail.com>
2019-05-13 20:57:12 -0700

    make very basic password generator

--- Elixir/password.ex
+++ Elixir/password.ex
@@ -1,5 +1,8 @@
 defmodule Password do
   def generate(length \\ 8) do
-    IO.puts length
+    character_range = (33..126)
+    Enum.reduce((1..length), [], fn (_, acc) ->
+      [Enum.random(character_range) | acc]
+    end) |> to_string
   end
 end
